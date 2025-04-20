adbb5e53622ed953b437ff36849b76f9ebee518e
unknown <arsandy.ja@gmail.com>
2025-04-20 22:40:03 +0700

    first time

---
+++ Tugas/Tugas_2_662023003.m
@@ -0,0 +1,32 @@
+clear 
+clc
+
+%membuat contoh Nested ( Decision Making ) dan While ( loop )
+
+%Nested menentukan jenis bilangan ganjil atau genap
+% angka = input('Masukkan sebuah angka: ');
+% 
+% if angka > 0
+%     if mod(angka,2) == 0
+%         disp('Bilangan positif genap');
+%     else
+%         disp('Bilangan positif ganjil');
+%     end
+% elseif angka < 0
+%     if mod(angka,2) == 0
+%         disp('Bilangan negatif genap');
+%     else
+%         disp('Bilangan negatif ganjil');
+%     end
+% else
+%     disp('Bilangan nol');
+% end
+
+%while urutan bilangan 1-5
+% i = 1;
+% 
+% while i <= 5
+%     fprintf('Angka: %d\n', i);
+%     i = i + 1;
+% end
+
